2177fc20a09144fc1954abdad2d5499891d31b50
Keith Hooks <khooks@gmail.com>
2017-08-27 15:15:20 -0400

    Update docstrings in rest.py

--- pybcm/rest.py
+++ pybcm/rest.py
@@ -36,10 +36,13 @@ from config import BCMConfig
 from const import *
 from legoutils import legoColors
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("pybcm.{}".format(__name__))
 
 
 def build_uri_template(url_key):
+    """Build a URI template using the url_key and constants from the API definition found
+    in const.py
+    """
     __skeleton = ''.join([API_PATH['base'], API_PATH[url_key]])
     __template = URITemplate(__skeleton)
     return __template
@@ -49,7 +52,6 @@ class RestClient:
     """Rest Client for Bricklink website"""
 
     def __init__(self, config=BCMConfig('../config/bcm.ini')):
-
         __consumer_key = config.consumer_key
         __consumer_secret = config.consumer_secret
         __token_key = config.token_key
@@ -58,7 +60,10 @@ class RestClient:
         self.auth = OAuth1(__consumer_key, __consumer_secret, __token_key, __token_secret)
 
     def __validate(self, itemtype=None, color=None, guidetype=None):
-
+        """
+        Validate some of the identities used in the Bricklink REST api. Items are optional
+        passing on the ones to validate. Errors are raised on invalid.
+        """
         if itemtype is not None and itemtype not in ITEM_TYPES:
             raise ValueError("Item Type {} is not valid".format(itemtype))
         if guidetype is not None and guidetype not in GUIDE_TYPES:
@@ -68,12 +73,17 @@ class RestClient:
             pass
 
     def get(self, url):
+        """Create request and get it for the passed url
+        :param url: URL to retrieve
+        :return response: The response
+        """
         response = requests.get(url, auth=self.auth)
         return response
 
     def get_item(self, itemid, itemtypeid):
         """
         /items/{type}/{no}
+        (see Bricklink API)
         """
         self.__validate(itemtype=itemtypeid)
         url = build_uri_template('item').expand(type=itemtypeid, no=itemid)
@@ -83,6 +93,7 @@ class RestClient:
     def get_item_image(self, itemid, itemtypeid, colorid):
         """
         /items/{type}/{no}
+        (see Bricklink API)
         """
         self.__validate(itemtype=itemtypeid, color=colorid)
         url = build_uri_template('item_image').expand(type=itemtypeid, no=itemid, color_id=colorid)
@@ -90,6 +101,10 @@ class RestClient:
         return self.get(url).json()['data']
 
     def get_supersets(self, itemid, itemtypeid):
+        """
+        /items/{type}/{no}/supersets
+        (see Bricklink API)
+        """
         self.__validate(itemtype=itemtypeid)
         url = build_uri_template('supersets').expand(type=itemtypeid, no=itemid)
         logger.info("Getting supersets from: {}".format(url))
@@ -98,6 +113,7 @@ class RestClient:
     def get_subsets(self, itemid, itemtypeid):
         """
         /items/{type}/{no}/subsets
+        (see Bricklink API)
         """
         self.__validate(itemtype=itemtypeid)
         url = build_uri_template('subsets').expand(type=itemtypeid, no=itemid)
@@ -105,7 +121,15 @@ class RestClient:
         return self.get(url).json()['data']
 
     def get_price_guide(self, itemid, itemtypeid, colorid, new_or_used='U', guide_type='sold'):
-
+        """
+        /items/{type}/{no}/price
+        :param itemid:
+        :param itemtypeid:
+        :param colorid:
+        :param new_or_used: 'N' or 'U'
+        :param guide_type: 'sold' or 'stock
+        :return data: JSON formatted price data (see Bricklink API)
+        """
         self.__validate(itemtype=itemtypeid, guidetype=guide_type)
         endpoint = build_uri_template('priceguide').expand(type=itemtypeid, no=itemid)
         params = {
@@ -129,17 +153,30 @@ class RestClient:
         return data
 
     def get_part_price_guide(self, itemid, colorid, new_or_used):
+        """
+        Get the price guide from the catalog
+        /items/{type}/{no}/price
+        :param itemid:
+        :param colorid:
+        :param new_or_used: 'N' or 'U'
+        :return data: JSON-formatted price guide (see Bricklink API)
+        """
         self.__validate(color=colorid)
         data = self.get_price_guide(itemid, 'PART', colorid, new_or_used=new_or_used)
         return data
 
     def get_known_colors(self, itemid, itemtypeid):
         """
-            GET	/items/{type}/{no}/colors
+        Get the list of known colors for a given item
+        /items/{type}/{no}/colors
+        :param itemid:
+        :param itemtypeid:
+        :return data: JSON-formatted color list (see Bricklink API)
         """
         url = build_uri_template('known_colors').expand(type=itemtypeid, no=itemid)
         logger.debug("Getting Known Colors from: {}".format(url))
-        return self.get(url).json()['data']
+        data = self.get(url).json()['data']
+        return data
 
 
 if __name__ == "__main__":
